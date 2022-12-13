99e2ba61125cf32c95cec3bebc29030cf95da5ae
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2022-12-13 13:28:43 +0000

    Added project skeleton

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import * as nunjucks from "nunjucks";
+import * as path from "path";
+import { router } from "./routes/routes";
+import { sessionMiddleware } from "./middleware/session.middleware";
+import cookieParser from "cookie-parser";
+import { logger } from "./utils/logger";
+
+
+const app = express();
+app.disable("x-powered-by");
+
+// view engine setup
+const nunjucksEnv = nunjucks.configure([
+  "views",
+  "node_modules/govuk-frontend/",
+  "node_modules/govuk-frontend/components/",
+], {
+  autoescape: true,
+  express: app,
+});
+
+nunjucksEnv.addGlobal("assetPath", process.env.CDN_HOST);
+nunjucksEnv.addGlobal("PIWIK_URL", process.env.PIWIK_URL);
+nunjucksEnv.addGlobal("PIWIK_SITE_ID", process.env.PIWIK_SITE_ID);
+
+app.enable("trust proxy");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "html");
+
+// apply middleware
+app.use(cookieParser());
+
+
+app.use("/xbrl_validate", router);
+
+
+logger.info("Account Validator Web has started");
+export default app;
---
+++ src/controllers/start.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { CHS_URL, PIWIK_START_GOAL_ID, EWF_URL } from "../config";
+import { Templates } from "../types/template.paths";
+
+export const get = (req: Request, res: Response) => {
+  return res.render(Templates.START, { CHS_URL,
+    PIWIK_START_GOAL_ID,
+    EWF_URL,
+    templateName: Templates.START });
+};
