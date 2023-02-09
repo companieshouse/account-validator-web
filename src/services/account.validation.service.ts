a161318d1868779ae3a82a8c395f38c4efbd3251
modhasanjay <121036699+modhasanjay@users.noreply.github.com>
2023-02-09 15:48:22 +0000

    Initial commit (WIP)

--- src/services/account.validation.service.ts
+++ src/services/account.validation.service.ts
@@ -1,3 +1,8 @@
+import { Resource } from "@companieshouse/api-sdk-node/dist";
+import ApiClient from "@companieshouse/api-sdk-node/dist/client";
+import { AccountValidatorResponse } from "@companieshouse/api-sdk-node/dist/services/account-validator/types";
+import { createPublicApiKeyClient } from "./api.service";
+
 export interface AccountValidationService {
     submit(file: Express.Multer.File): Promise<AccountValidationResult>
 }
@@ -16,7 +21,15 @@ export type AccountValidationResult
     = SuccessValidationResult
     | FailureValidationResult;
 
+export function mapResponseType(accountValidatorResource: Resource<AccountValidatorResponse>): AccountValidationResult{
+    const accountValidatorResponse = (accountValidatorResource as Resource<AccountValidatorResponse>).resource!;
 
+    switch (accountValidatorResponse.requestStatus.status){
+            case "OK": return { status: "success" };
+            case "FAILED":  return { status: "failure", reasons: accountValidatorResponse.requestStatus.result.errorMessages };
+            default: throw "This is not possbile. Only possible options are OK and FAILED";
+    }
+}
 // TODO: implement an API based validator that will upload a file to S3 then forward the url to the API to validate.
 
 // Only used for testing. Should be removed once API based validator is implemented
@@ -42,4 +55,24 @@ export class DummyValidator implements AccountValidationService {
     }
 }
 
-export const accountValidatorService = new DummyValidator({ 'accounts_success.zip': { status: 'success', imageUrl: 'https://www.google.co.uk' } });
+export class AccountValidator implements AccountValidationService {
+
+    constructor(private apiClient: ApiClient = createPublicApiKeyClient()) {
+
+    }
+
+    async submit(file: Express.Multer.File): Promise<any> {
+        // TODO: Upload to S3 using File Transfer Service(FTS)
+        const fileId = "fileId"; // Replace this with the actual fileId returned from FTS
+        const requestPayload = { "fileName": file.filename, "id": fileId };
+        const accountValidatorService = this.apiClient.accountValidatorService;
+        const accountValidatorResponse = await accountValidatorService.postFileForValidation(requestPayload);
+
+        if (accountValidatorResponse.httpStatusCode !== 200){
+            throw accountValidatorResponse; // If the status code is not 200, the return type is ApiErrorResponse
+        }
+        return mapResponseType(accountValidatorResponse as Resource<AccountValidatorResponse>);
+    }
+}
+
+export const accountValidatorService = new AccountValidator();
