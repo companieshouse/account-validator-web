5f6f562cefc7069f19156d15e4a42d59ef64a43c
rmcmurray-ch <rmcmurray@companieshouse.gov.uk>
2023-04-18 09:44:17 +0100

    fix for tests

--- src/services/account.validation.service.ts
+++ src/services/account.validation.service.ts
@@ -138,7 +138,7 @@ export class AccountValidator implements AccountValidationService {
      * @throws If the API returns a non-200 status code, the returned value is an instance of `ApiErrorResponse`
      */
     async check(id: string): Promise<AccountValidationResult> {
-        const accountValidatorService = this.privateApiClient.accountValidorService;
+        const accountValidatorService = this.apiClient.accountValidatorService;
         const accountValidatorResponse =
             await accountValidatorService.getFileValidationStatus(id);
         if (accountValidatorResponse.httpStatusCode !== 200) {
@@ -163,7 +163,7 @@ export class AccountValidator implements AccountValidationService {
         if (fileId.resource?.id) {
 
             const requestPayload = { fileName: file.originalname, id: fileId.resource?.id };
-            const accountValidatorService = this.privateApiClient.accountValidorService;
+            const accountValidatorService = this.apiClient.accountValidatorService;
 
             const accountValidatorResponse =
                 await accountValidatorService.postFileForValidation(requestPayload);
