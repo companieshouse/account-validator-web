0a401bddb56432d1c79f637b921819822eec3dde
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2023-04-24 14:47:01 +0100

    Added some logging

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -3,11 +3,14 @@ import { createApiClient } from "@companieshouse/api-sdk-node/dist";
 import ApiClient from "@companieshouse/api-sdk-node/dist/client";
 import { createPrivateApiClient } from "private-api-sdk-node";
 import PrivateApiClient from 'private-api-sdk-node/dist/client';
+import { logger } from "../utils/logger";
 
 export const createPublicApiKeyClient = (): ApiClient => {
     return createApiClient(CHS_API_KEY, undefined, API_URL);
 };
 
 export const createPrivateApiKeyClient = (): PrivateApiClient => {
+    logger.info(`Creating private API client with key ${CHS_INTERNAL_API_KEY.slice(0, 5)} (first 5 characters of key actual length ${CHS_INTERNAL_API_KEY.length})`);
+
     return createPrivateApiClient(CHS_INTERNAL_API_KEY, undefined, INTERNAL_API_URL);
 };
