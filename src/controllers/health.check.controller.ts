7259c379e470273d50b7ef6eaae21dfa7d847b57
wdhda-ch <awoodhead@companieshouse.gov.uk>
2023-04-27 15:48:33 +0100

    correct typo

--- src/controllers/health.check.controller.ts
+++ src/controllers/health.check.controller.ts
@@ -1,9 +1,9 @@
-import { Response, Router } from "express";
+import { Response, Router, Request } from "express";
 import { logger } from "../utils/logger";
 
 export const healthCheckController = Router({ mergeParams: true });
 
-function healthCheck(res: Response) {
+function healthCheck(req: Request, res: Response) {
     logger.debug(`health check triggered`);
     return res.sendStatus(200);
 }
