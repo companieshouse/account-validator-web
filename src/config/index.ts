31538ab7c685f01e2c4843075c753a770c6de901
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2023-04-28 15:12:53 +0100

    Fixed get env var func

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,7 @@
  */
 
 const getEnvironmentVariable = (key: string, defaultValue?: any): string => {
-    const isMandatory = !defaultValue;
+    const isMandatory = defaultValue === undefined;
     const value: string = process.env[key] || "";
 
     if (!value && isMandatory) {
