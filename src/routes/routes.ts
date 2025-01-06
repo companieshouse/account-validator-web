87bc904a9634067e65139d31e1d69ba3b8e5c77d
wdhda-ch <127737882+wdhda-ch@users.noreply.github.com>
2025-01-06 13:40:03 +0000

    Add extra middleware to submit uri

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -8,13 +8,27 @@ import { healthCheckController } from "../controllers/health.check.controller";
 import { errorController } from "../controllers/error.controller";
 import { Urls } from "../constants";
 import { progressController } from "../controllers/progress.controller";
+import { authenticationMiddleware } from "../middleware/authentication.middleware";
+import { COOKIE_CONFIG, sessionMiddleware } from "../middleware/session.middleware";
+import { EnsureSessionCookiePresentMiddleware, SessionStore } from "@companieshouse/node-session-handler";
+import { CACHE_SERVER } from "../config";
+import Redis from "ioredis";
 
-export const router = Router();
+const setupSessionStore = () => {
+    const redis = new Redis(`redis://${CACHE_SERVER}`);
+    return new SessionStore(redis);
+};
+
+const router = Router();
+const sessionStore = setupSessionStore();
+
+router.use(Urls.HEALTH_CHECK_SUFFIX, healthCheckController);
 
 router.use('/', startController);
 router.use('/render/:id', renderController);
-router.use(Urls.SUBMIT_SUFFIX, submitController);
+router.use(Urls.SUBMIT_SUFFIX, sessionMiddleware(sessionStore), EnsureSessionCookiePresentMiddleware(COOKIE_CONFIG), authenticationMiddleware, submitController);
 router.use(`${Urls.RESULT_SUFFIX}/:id`, resultController);
 router.use(Urls.PROGRESS_SUFFIX, progressController);
-router.use(Urls.HEALTH_CHECK_SUFFIX, healthCheckController);
 router.use(Urls.ERROR_SUFFIX, errorController);
+
+export default router;
