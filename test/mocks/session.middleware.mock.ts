3f312dfea31a3043cfe08768505de118f194f93e
wdhda-ch <127737882+wdhda-ch@users.noreply.github.com>
2025-01-06 13:40:39 +0000

    adding and updating middleware mocks

---
+++ test/mocks/session.middleware.mock.ts
@@ -0,0 +1,26 @@
+jest.mock("ioredis");
+jest.mock("../../src/middleware/session.middleware");
+
+import { NextFunction, Request, Response } from "express";
+import { sessionMiddleware } from "../../src/middleware/session.middleware";
+import { Session } from "@companieshouse/node-session-handler";
+import { getSessionRequest } from "./session.mock";
+
+// get handle on mocked function
+export const mockSessionMiddleware = sessionMiddleware as jest.Mock;
+
+export let mockSession = new Session();
+
+export const mockRequestSession = () => mockSession = getSessionRequest();
+
+// tell the mock what to return
+mockSessionMiddleware.mockImplementation((_) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        req.session = mockSession;
+        next();
+    };
+});
+
+export function resetMockSession() {
+    mockSession = new Session();
+}
