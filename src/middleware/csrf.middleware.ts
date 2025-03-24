cbd240ba13f0ef36b1300a53c0157916c9254d2a
NeilShadrach <neil.shadrach@corryn.com>
2025-03-24 08:39:01 +0000

    Revert to simpler createCsrfProtectionMiddleware

--- src/middleware/csrf.middleware.ts
+++ src/middleware/csrf.middleware.ts
@@ -20,37 +20,9 @@ export const csrfErrorHandler = (
     });
 };
 
-export const createCsrfProtectionMiddleware = (sessionStore: SessionStore) => {
-    console.log('NSDBG CSRF enter/return sessionStore: ' + sessionStore);
-    const csrfMiddleware = CsrfProtectionMiddleware({
-        sessionStore: sessionStore,
-        enabled: true,
-        sessionCookieName: COOKIE_NAME
-    });
 
-    return (req, res, next) => {
-        console.log(`NSDBG CSRF middleware called for ${req.path}`);
-        console.log('NSDBG before CSRF middleware - session: ' + JSON.stringify(req.session));
-        csrfMiddleware(req, res, () => {
-            console.log('NSDBG after CSRF middleware session contents: ' + JSON.stringify(req.session));
-            console.log('NSDBG CSRF token in res.locals after middleware for ' + req.path + ': ' + res.locals._csrf);
-            if (!res.locals._csrf) {
-                console.log('NSDBG Manually generating CSRF token');
-                res.locals._csrf = req.csrfToken ? req.csrfToken() : 'MISSING_CSRF_TOKEN';
-            }
-            console.log('NSDBG CSRF token after manual generation: ' + res.locals._csrf);
-            next();
-        });
-    };
-    // return CsrfProtectionMiddleware({
-    //    sessionStore: sessionStore,
-    //    enabled: true,
-    //    sessionCookieName: COOKIE_NAME
-    // });
-};
-
-// export const createCsrfProtectionMiddleware = (sessionStore: SessionStore) => CsrfProtectionMiddleware({
-//    sessionStore: sessionStore,
-//    enabled: true,
-//    sessionCookieName: COOKIE_NAME
-// });
+export const createCsrfProtectionMiddleware = (sessionStore: SessionStore) => CsrfProtectionMiddleware({
+    sessionStore: sessionStore,
+    enabled: true,
+    sessionCookieName: COOKIE_NAME
+});
